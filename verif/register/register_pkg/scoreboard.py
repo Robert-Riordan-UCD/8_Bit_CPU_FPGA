40cde7d7a221c4257eac53e800987748710c7e83
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-07-13 12:09:17 +0100

    Register scoreboard running correctly

--- verif/register/register_pkg/scoreboard.py
+++ verif/register/register_pkg/scoreboard.py
@@ -1,28 +1,25 @@
-from pyuvm import uvm_component, uvm_analysis_export
+from pyuvm import uvm_subscriber
 from cocotb.binary import BinaryValue
 
-class Scoreboard(uvm_component):
+class Scoreboard(uvm_subscriber):
     def __init__(self, parent, name="scoreboard"):
         super().__init__(name, parent)
         self.logger.info("Init SCB")
-        # self.analysis_export = uvm_analysis_export("analysis export", self)
+        self.expected_value = 'xxxxxxxx'
     
     def write(self, op):
         self.logger.info("Write SCB")
-        # expected_bus
 
-        expected_value = op.bus_driver if op.read_from_bus == 1 and op.write_to_bus == 0 else op.previous_value
-        self.logger.warning(f"Read {op.read_from_bus}, Write {op.write_to_bus}")
-        self.logger.warning(f"Expect {expected_value}, Got {op.value}")
-        # result = op.a - op.b if op.subtract else op.a + op.b
-        # expected_bus = result % 0xFF if op.out else BinaryValue(value="zzzzzzzz", n_bits=8)
+        if op.rst == 1:
+            self.expected_value = 0
 
-        # assert op.bus == expected_bus, f"Bus ERROR: got {op.bus} expected {expected_bus}"
-
-        # if op.flags_in:
-        #     assert op.carry == (result > 0xFF), "Carry ERROR"
-        #     assert op.zero == (expected_bus == 0), "Zero ERROR"
+        # Check value
+        assert self.expected_value == op.value, f"ERROR value: expected {self.expected_value}, actual {op.value}"
     
-    # def connect_phase(self):
-    #     self.logger.info("Connect SCB")
-    #     self.analysis_export.write = self.write
+        # Check bus
+        if op.write_to_bus == 1 and op.read_from_bus == 0 and op.rst == 0:
+            assert op.bus == self.expected_value, f"ERROR bus: expected {self.expected_value}, actual {op.bus}"
+
+        if op.read_from_bus == 1 and op.write_to_bus == 0 and op.rst == 0:
+            self.expected_value = op.bus_driver.value
+    

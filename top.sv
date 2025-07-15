54acde2098fdf9b0f2c1052af8b9912b95ebe20c
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-07-15 17:07:48 +0100

    Added all current modules to top

--- top.sv
+++ top.sv
@@ -1,5 +1,8 @@
 `include "src/clock.sv"
 `include "src/program_counter.sv"
+`include "src/memory_address_register.sv"
+`include "src/register.sv"
+`include "src/alu.sv"
 
 /* This module connects each of the major CPU components together
  * and provides an inteface to the FPGA IO
@@ -34,6 +37,9 @@ module top (
     logic [7:0] bus;
     assign bus = pc_out ? pc_bus : 8'b0;
 
+    logic [7:0] a_reg_value;
+    logic [7:0] b_reg_value;
+
     // Core modules
     clock u_clock (
         .sys_clk(clk),
@@ -42,7 +48,7 @@ module top (
         .cpu_clk(cpu_clk)
     );
 
-    program_counter u_pc(
+    program_counter u_pc (
         .clk(cpu_clk),
         .rst(reset),
         .inc(pc_inc),
@@ -50,6 +56,62 @@ module top (
         .out(pc_out),
         .bus(pc_bus)
     );
+
+    memory_address_register u_mar (
+        .clk(cpu_clk),
+        .rst(reset),
+        .read_from_bus(),
+        .manual_mode(),
+        .manual_read(),
+        .manual_switches(),
+        .bus(),
+        .address()
+    );
+
+    register u_a_reg (
+        .clk(cpu_clk),
+        .rst(reset),
+        .read_from_bus(),
+        .write_to_bus(),
+        .bus(),
+        .value(a_reg_value)
+    );
+
+    // ram u_ram();
+
+    alu u_alu (
+        .clk(cpu_clk),
+        .rst(reset),
+        .a(a_reg_value),
+        .b(b_reg_value),
+        .out(),
+        .subtract(),
+        .flags_in(),
+        .bus(),
+        .carry(),
+        .zero()
+    );
+
+    register #(8'h0F) u_i_reg (
+        .clk(cpu_clk),
+        .rst(reset),
+        .read_from_bus(),
+        .write_to_bus(),
+        .bus(),
+        .value()
+    );
+    
+    register u_b_reg (
+        .clk(cpu_clk),
+        .rst(reset),
+        .read_from_bus(),
+        .write_to_bus(),
+        .bus(),
+        .value(b_reg_value)
+    );
+    
+    // control u_control();
+    // output_module u_output();
 
     // FPGA IO connections
     assign led = ~{bus[3:0], 1'b0, cpu_clk};

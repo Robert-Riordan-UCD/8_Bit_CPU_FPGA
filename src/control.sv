d30e7ee8eed152b35fce9d5c0a4b8b65b4f922cd
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-07-19 16:39:21 +0100

    Control interface and micorcode coutner setup

---
+++ src/control.sv
@@ -0,0 +1,48 @@
+module control (
+    input clk,
+    input rst,
+
+    input [3:0] instruction;
+
+    input alu_carry,
+    input alu_zero,
+
+    output clk_halt,
+    
+    output pc_inc,
+    output pc_jump,
+    output pc_out,
+    
+    output a_reg_read_from_bus,
+    output a_reg_write_to_bus,
+
+    output b_reg_read_from_bus,
+    output b_reg_write_to_bus,
+
+    output i_reg_read_from_bus,
+    output i_reg_write_to_bus,
+
+    output mar_read_from_bus,
+
+    output ram_read_from_bus,
+    output ram_write_to_bus,
+
+    output alu_out,
+    output alu_subtract,
+    output alu_flags_in,
+
+    output out_en,
+);
+
+    localparam STEP_MAX = 6;
+    logic [2:0] step_counter;
+
+    always_ff @(negedge clk) begin
+        if (rst || step_counter == STEP_MAX) begin
+            step_counter <= 0;
+        end else begin
+            step_counter <= step_counter + 1;
+        end
+    end
+
+endmodule

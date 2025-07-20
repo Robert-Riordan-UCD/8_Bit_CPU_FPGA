c7539e10ee2dce34f5de3c93c95f92a25e7d1996
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-07-20 12:03:54 +0100

    Added all remaing non-conditional instruction sequences

--- verif/control/control_pkg/sequences.py
+++ verif/control/control_pkg/sequences.py
@@ -12,6 +12,10 @@ class TestAllSeq(uvm_sequence):
         store_a = StoreA("store A")
         add = Add("add")
         sub = Sub("sub")
+        load_im = LoadIm("load imediate")
+        jump = Jump("jump")
+        out = Out("out")
+        halt = Halt("halt")
         # random = Random("random")
 
         # await all_ins.start(self.sequencer)
@@ -20,6 +24,10 @@ class TestAllSeq(uvm_sequence):
         await store_a.start(self.sequencer)
         await add.start(self.sequencer)
         await sub.start(self.sequencer)
+        await load_im.start(self.sequencer)
+        await jump.start(self.sequencer)
+        await out.start(self.sequencer)
+        await halt.start(self.sequencer)
         # await random.start(self.sequencer)
 
 class Reset(uvm_sequence):
@@ -86,6 +94,70 @@ class Sub(uvm_sequence):
             SeqItem(name="sub step 4", instruction=0b0011, expected_output=[Signal.ALU_OUT, Signal.ALU_FLG, Signal.ALU_SUB, Signal.A_RD]),
             SeqItem(name="sub step 5", instruction=0b0011),
             SeqItem(name="sub step 6", instruction=0b0011),
+        ]
+
+        for seq in seqs:
+            await self.start_item(seq)
+            await self.finish_item(seq)
+
+class LoadIm(uvm_sequence):
+    async def body(self):
+        seqs = [
+            SeqItem(name="load imediate step 0", instruction=0b0101, expected_output=[Signal.PC_OUT, Signal.MAR_RD]),
+            SeqItem(name="load imediate step 1", instruction=0b0101, expected_output=[Signal.RAM_WRT, Signal.I_RD, Signal.PC_INC]),
+            SeqItem(name="load imediate step 2", instruction=0b0101, expected_output=[Signal.I_WRT, Signal.A_RD]),
+            SeqItem(name="load imediate step 3", instruction=0b0101),
+            SeqItem(name="load imediate step 4", instruction=0b0101),
+            SeqItem(name="load imediate step 5", instruction=0b0101),
+            SeqItem(name="load imediate step 6", instruction=0b0101),
+        ]
+
+        for seq in seqs:
+            await self.start_item(seq)
+            await self.finish_item(seq)
+
+class Jump(uvm_sequence):
+    async def body(self):
+        seqs = [
+            SeqItem(name="jump step 0", instruction=0b0110, expected_output=[Signal.PC_OUT, Signal.MAR_RD]),
+            SeqItem(name="jump step 1", instruction=0b0110, expected_output=[Signal.RAM_WRT, Signal.I_RD, Signal.PC_INC]),
+            SeqItem(name="jump step 2", instruction=0b0110, expected_output=[Signal.I_WRT, Signal.PC_JMP]),
+            SeqItem(name="jump step 3", instruction=0b0110),
+            SeqItem(name="jump step 4", instruction=0b0110),
+            SeqItem(name="jump step 5", instruction=0b0110),
+            SeqItem(name="jump step 6", instruction=0b0110),
+        ]
+
+        for seq in seqs:
+            await self.start_item(seq)
+            await self.finish_item(seq)
+
+class Out(uvm_sequence):
+    async def body(self):
+        seqs = [
+            SeqItem(name="out step 0", instruction=0b1110, expected_output=[Signal.PC_OUT, Signal.MAR_RD]),
+            SeqItem(name="out step 1", instruction=0b1110, expected_output=[Signal.RAM_WRT, Signal.I_RD, Signal.PC_INC]),
+            SeqItem(name="out step 2", instruction=0b1110, expected_output=[Signal.A_WRT, Signal.OUT_EN]),
+            SeqItem(name="out step 3", instruction=0b1110),
+            SeqItem(name="out step 4", instruction=0b1110),
+            SeqItem(name="out step 5", instruction=0b1110),
+            SeqItem(name="out step 6", instruction=0b1110),
+        ]
+
+        for seq in seqs:
+            await self.start_item(seq)
+            await self.finish_item(seq)
+
+class Halt(uvm_sequence):
+    async def body(self):
+        seqs = [
+            SeqItem(name="halt step 0", instruction=0b1111, expected_output=[Signal.PC_OUT, Signal.MAR_RD]),
+            SeqItem(name="halt step 1", instruction=0b1111, expected_output=[Signal.RAM_WRT, Signal.I_RD, Signal.PC_INC]),
+            SeqItem(name="halt step 2", instruction=0b1111, expected_output=[Signal.CLK_HLT]),
+            SeqItem(name="halt step 3", instruction=0b1111),
+            SeqItem(name="halt step 4", instruction=0b1111),
+            SeqItem(name="halt step 5", instruction=0b1111),
+            SeqItem(name="halt step 6", instruction=0b1111),
         ]
 
         for seq in seqs:

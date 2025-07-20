f0be846f6000d1b7cd58db73dd044c0cdfb1c9b6
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-07-20 15:38:38 +0100

    Added control output signals coverage

--- verif/control/control_pkg/coverage.py
+++ verif/control/control_pkg/coverage.py
@@ -20,6 +20,24 @@ class Coverage(uvm_subscriber):
         self.ins_count = 0
         self.instructions = {}
 
+        self.clk_halt = set()
+        self.pc_inc = set()
+        self.pc_jump = set()
+        self.pc_out = set()
+        self.a_reg_read_from_bus = set()
+        self.a_reg_write_to_bus = set()
+        self.b_reg_read_from_bus = set()
+        self.b_reg_write_to_bus = set()
+        self.i_reg_read_from_bus = set()
+        self.i_reg_write_to_bus = set()
+        self.mar_read_from_bus = set()
+        self.ram_read_from_bus = set()
+        self.ram_write_to_bus = set()
+        self.alu_out = set()
+        self.alu_subtract = set()
+        self.alu_flags_in = set()
+        self.out_en = set()
+
     def write(self, op):
         self.logger.info("Write COV")
         
@@ -40,7 +58,23 @@ class Coverage(uvm_subscriber):
             if not (i := intxz(op.instruction)) is None:
                 self.instructions[i] = self.instructions.get(i, 0) + 1
 
-
+        self.clk_halt.add(int(op.clk_halt.value))
+        self.pc_inc.add(int(op.pc_inc.value))
+        self.pc_jump.add(int(op.pc_jump.value))
+        self.pc_out.add(int(op.pc_out.value))
+        self.a_reg_read_from_bus.add(int(op.a_reg_read_from_bus.value))
+        self.a_reg_write_to_bus.add(int(op.a_reg_write_to_bus.value))
+        self.b_reg_read_from_bus.add(int(op.b_reg_read_from_bus.value))
+        self.b_reg_write_to_bus.add(int(op.b_reg_write_to_bus.value))
+        self.i_reg_read_from_bus.add(int(op.i_reg_read_from_bus.value))
+        self.i_reg_write_to_bus.add(int(op.i_reg_write_to_bus.value))
+        self.mar_read_from_bus.add(int(op.mar_read_from_bus.value))
+        self.ram_read_from_bus.add(int(op.ram_read_from_bus.value))
+        self.ram_write_to_bus.add(int(op.ram_write_to_bus.value))
+        self.alu_out.add(int(op.alu_out.value))
+        self.alu_subtract.add(int(op.alu_subtract.value))
+        self.alu_flags_in.add(int(op.alu_flags_in.value))
+        self.out_en.add(int(op.out_en.value))
 
     def report_phase(self):
         self.logger.info("Report COV")
@@ -50,3 +84,55 @@ class Coverage(uvm_subscriber):
                 self.logger.info(f"Coverage: Instruction {i} covered {self.instructions[i]} times")
             else:
                 self.logger.error(f"Coverage MISS: Instruction {i} missed")
+
+        assert 0 in self.clk_halt, "HALT never 0"
+        assert 1 in self.clk_halt, "HALT never 1"
+        
+        assert 0 in self.pc_inc, "PC INC never 0"
+        assert 1 in self.pc_inc, "PC INC never 1"
+        
+        assert 0 in self.pc_out, "PC OUT never 0"
+        assert 1 in self.pc_out, "PC out never 1"
+        
+        assert 0 in self.pc_jump, "PC JUMP never 0"
+        assert 1 in self.pc_jump, "PC JUMP never 1"
+        
+        assert 0 in self.a_reg_read_from_bus, "A READ never 0"
+        assert 1 in self.a_reg_read_from_bus, "A READ never 1"
+        
+        assert 0 in self.a_reg_write_to_bus, "A WRITE never 0"
+        assert 1 in self.a_reg_write_to_bus, "A WRITE never 1"
+        
+        assert 0 in self.b_reg_read_from_bus, "B READ never 0"
+        assert 1 in self.b_reg_read_from_bus, "B READ never 1"
+        
+        assert 0 in self.b_reg_write_to_bus, "B WRITE never 0"
+        # assert 1 in self.b_reg_write_to_bus, "B WRITE never 1"
+        assert not 1 in self.b_reg_write_to_bus, "B WRITE 1 but no instruction to set B WRITE"
+        
+        assert 0 in self.i_reg_read_from_bus, "I READ never 0"
+        assert 1 in self.i_reg_read_from_bus, "I READ never 1"
+
+        assert 0 in self.i_reg_write_to_bus, "I WRITE never 0"
+        assert 1 in self.i_reg_write_to_bus, "I WRITE never 1"
+
+        assert 0 in self.mar_read_from_bus, "MAR READ never 0"
+        assert 1 in self.mar_read_from_bus, "MAR READ never 1"
+
+        assert 0 in self.ram_read_from_bus, "RAM READ never 0"
+        assert 1 in self.ram_read_from_bus, "RAM READ never 1"
+        
+        assert 0 in self.ram_write_to_bus, "RAM WRITE never 0"
+        assert 1 in self.ram_write_to_bus, "RAM WRITE never 1"
+        
+        assert 0 in self.alu_out, "ALU OUT never 0"
+        assert 1 in self.alu_out, "ALU OUT never 1"
+        
+        assert 0 in self.alu_flags_in, "ALU FLAGS IN never 0"
+        assert 1 in self.alu_flags_in, "ALU FLAGS IN never 1"
+        
+        assert 0 in self.alu_subtract, "ALU SUB never 0"
+        assert 1 in self.alu_subtract, "ALU SUB never 1"
+        
+        assert 0 in self.out_en, "OUT EN never 0"
+        assert 1 in self.out_en, "OUT EN never 1"

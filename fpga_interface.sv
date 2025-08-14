f27414665aedf5c9f4bb6be8a2685efed090f234
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-08-14 18:05:06 +0200

    Control working on the FPGA

--- fpga_interface.sv
+++ fpga_interface.sv
@@ -14,23 +14,15 @@ module fpga_interface (
     input logic clk,
     input logic rst_n,
 
-    /* Clock test */
+    /* Clock */
     input logic clk_mode,
     input logic clk_pulse,
-    input logic clk_halt,
     output logic clk_output,
 
-    /* Program counter test */
-    input logic pc_inc,
-    input logic pc_jump,
-    /* I register */
-    input i_read_from_bus,
-
     /* Bus output */
-    input logic pc_out,
-    input logic i_write_to_bus,
     output logic [7:0] bus_output,
 
+    /* Debug leds */
     output logic [5:0] led
 );
     /* Debug LEDs */
@@ -40,13 +32,40 @@ module fpga_interface (
     logic rst;
     assign rst = ~rst_n;
 
-    /* Clock test */
+    logic cpu_clk;
+
+    /* Control signals */
+    logic alu_carry;
+    logic alu_zero;
+    logic clk_halt;
+    logic pc_inc;
+    logic pc_jump;
+    logic pc_out;
+    logic a_reg_read_from_bus;
+    logic a_reg_write_to_bus;
+    logic b_reg_read_from_bus;
+    logic b_reg_write_to_bus;
+    logic i_reg_read_from_bus;
+    logic i_reg_write_to_bus;
+    logic mar_read_from_bus;
+    logic ram_read_from_bus;
+    logic ram_write_to_bus;
+    logic alu_out;
+    logic alu_subtract;
+    logic alu_flags_in;
+    logic out_en;
+
+    /* Buses */
+    logic [7:0] pc_bus_out;
+    logic [7:0] i_bus_out;
+    
+    /* Other data */
+    logic [7:0] instruction;
+
+    /* Clock */
     // Pin 69 - resistor - led - VCC (Output)
     // Pin 68 - slide switch center - VCC/GND (Mode)
     // Pin 57 - button - VCC (Pulse)
-    // Pin 56 - button - VCC (Halt)
-    logic cpu_clk;
-
     clock u_clock (
         .sys_clk(clk),
         .mode(clk_mode),
@@ -57,10 +76,7 @@ module fpga_interface (
 
     assign clk_output = ~cpu_clk;
 
-    /* Program counter test */
-    // Pin 34 - button - VCC (INC)
-    // Pin 33 - button - VCC (JMP)
-    logic [7:0] pc_bus_out;
+    /* Program counter */
     program_counter u_pc (
         .clk(cpu_clk),
         .rst(rst),
@@ -70,20 +86,42 @@ module fpga_interface (
         .bus_out(pc_bus_out)
     );
 
-    /* Instruction register test */
-    // Pin 30 - button - VCC (READ)
-    logic [7:0] i_bus_out;
-    logic [7:0] instruction;
+    /* Instruction register */
     register #(8'h0F) u_i_reg (
         .clk(cpu_clk),
         .rst(rst),
-        .read_from_bus(i_read_from_bus),
+        .read_from_bus(i_reg_read_from_bus),
         .bus_in(bus_out),
         .bus_out(i_bus_out),
         .value(instruction)
     );
 
-    /* Bus output test */
+    control u_control(
+        .clk(cpu_clk),
+        .rst(rst),
+        .instruction(instruction[7:4]),
+        .alu_carry(alu_carry),
+        .alu_zero(alu_zero),
+        .clk_halt(clk_halt),
+        .pc_inc(pc_inc),
+        .pc_jump(pc_jump),
+        .pc_out(pc_out),
+        .a_reg_read_from_bus(a_reg_read_from_bus),
+        .a_reg_write_to_bus(a_reg_write_to_bus),
+        .b_reg_read_from_bus(b_reg_read_from_bus),
+        .b_reg_write_to_bus(b_reg_write_to_bus),
+        .i_reg_read_from_bus(i_reg_read_from_bus),
+        .i_reg_write_to_bus(i_reg_write_to_bus),
+        .mar_read_from_bus(mar_read_from_bus),
+        .ram_read_from_bus(ram_read_from_bus),
+        .ram_write_to_bus(ram_write_to_bus),
+        .alu_out(alu_out),
+        .alu_subtract(alu_subtract),
+        .alu_flags_in(alu_flags_in),
+        .out_en(out_en)
+    );
+
+    /* Bus output */
     // Pins 40, 35, 41, 42, 51, 52, 53, 54, 55 (BUS)
     // Each pin - resistor - led - VCC
     // Pin 29 - button - VCC (PC write to bus)

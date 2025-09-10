57fec1225f920ed4803fd5aff4af52b617636972
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-09-10 16:55:29 +0200

    Added toggle and multiply programs to bootloader

--- src/bootloader.sv
+++ src/bootloader.sv
@@ -45,22 +45,52 @@ module bootloader (
         // Data
         programs[0][15] =    {8'b00000001};
 
-        /* Fibonacci */
+        /* Alternate between 0 and F */
         // Program
         programs[1][0] = {`LOAD_IM, 4'b0000};
-        programs[1][1] = {`ADD,     4'b1111};
-        programs[1][2] = {`JUMPC,   4'b1010};
-        programs[1][3] = {`OUT,     4'b0000};
-        programs[1][4] = {`STORE_A, 4'b1101};
-        programs[1][5] = {`LOAD_A,  4'b1110};
-        programs[1][6] = {`STORE_A, 4'b1111};
-        programs[1][7] = {`LOAD_A,  4'b1101};
-        programs[1][8] = {`STORE_A, 4'b1110};
-        programs[1][9] = {`JUMP,    4'b0001};
-        programs[1][10]= {`HALT,    4'b0000};
+        programs[1][1] = {`OUT,     4'b0000};
+        programs[1][2] = {`NOOP,    4'b0000}; // NOOP so each digit get equal cycles
+        programs[1][3] = {`LOAD_IM, 4'b1111};
+        programs[1][4] = {`OUT,     4'b0000};
+        programs[1][5] = {`JUMP,    4'b0000};
+
+        /* Fibonacci */
+        // Program
+        programs[2][0] = {`LOAD_IM, 4'b0000};
+        programs[2][1] = {`ADD,     4'b1111};
+        programs[2][2] = {`JUMPC,   4'b1010};
+        programs[2][3] = {`OUT,     4'b0000};
+        programs[2][4] = {`STORE_A, 4'b1101};
+        programs[2][5] = {`LOAD_A,  4'b1110};
+        programs[2][6] = {`STORE_A, 4'b1111};
+        programs[2][7] = {`LOAD_A,  4'b1101};
+        programs[2][8] = {`STORE_A, 4'b1110};
+        programs[2][9] = {`JUMP,    4'b0001};
+        programs[2][10]= {`HALT,    4'b0000};
         // Data
-        programs[1][14] = {8'b00000001};
-        programs[1][15] = {8'b00000000};
+        programs[2][14] = {8'b00000001};
+        programs[2][15] = {8'b00000000};
+
+        /* Multiply 6 by 11 */
+        // = 66 -> 0x42
+        // Program
+        programs[3][0] = {`LOAD_A,  4'd15};
+        programs[3][1] = {`STORE_A, 4'd13};
+        programs[3][2] = {`LOAD_A,  4'd13};
+        programs[3][3] = {`OUT,     4'b0};
+        programs[3][4] = {`ADD,     4'd15};
+        programs[3][5] = {`STORE_A, 4'd13};
+        programs[3][6] = {`LOAD_A,  4'd14};
+        programs[3][7] = {`SUB,     4'd12};
+        programs[3][8] = {`JUMPZ,   4'd11};
+        programs[3][9] = {`STORE_A, 4'd14};
+        programs[3][10] = {`JUMP,   4'd2};
+        programs[3][11] = {`HALT,   4'b0};
+        // Data
+        programs[3][12] = 8'b1;
+        programs[3][13] = 8'b0; // SUM
+        programs[3][14] = 8'd11; // B
+        programs[3][15] = 8'd6; // A
     end
 
     always_ff @(posedge clk or posedge rst) begin
@@ -94,35 +124,3 @@ module bootloader (
     end
     
 endmodule
-
- /* Alternate between 8 and 1 */
-        // data[0] = 8'b01011000; // LOAD IM 8
-        // data[1] = 8'b11100000; // OUT
-        // data[2] = 8'b01010001; // LOAD IM 1
-        // data[3] = 8'b11100000; // OUT
-        
-        // data[4] = 8'b01100000; // JMP 0
-    
-        // /* Multiply 6 by 11 */
-        // data[0] = {`LOADA,  4'd15};
-        // data[1] = {`STORE_A, 4'd13};
-        
-        // data[2] = {`LOADA,  4'd13};
-        // data[3] = {`OUT,    4'b0};
-        // data[4] = {`ADD,    4'd15};
-        // data[5] = {`STORE_A, 4'd13};
-        
-        // data[6] = {`LOADA,  4'd14};
-        // data[7] = {`SUB,    4'd12};
-        // data[8] = {`JUMPZ,  4'd11};
-        
-        // data[9] = {`STORE_A, 4'd14};
-        // data[10] = {`JUMP,  4'd2};
-        
-        // data[11] = {`HALT,  4'b0};
-        
-        // data[12] = 8'b1; // 1
-        // data[13] = 8'b0; // SUM
-        // data[14] = 8'd11; // B
-        // data[15] = 8'd6; // A
-
